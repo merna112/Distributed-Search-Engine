4526f540dbace887896cc409714e8a3308496d1e
merna112 <mernaa112@email.com>
2026-03-07 02:15:06 +0200

    feat: index model schema

---
+++ backend/models/Index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const documentEntrySchema = new mongoose.Schema(
+    {
+        url: { type: String, required: true },
+        tf: { type: Number, required: true },          // Term Frequency in this doc
+        positions: { type: [Number], default: [] },    // Character/word positions
+        totalTerms: { type: Number, default: 0 },      // Total terms in document
+    },
+    { _id: false }
+);
+
+const indexSchema = new mongoose.Schema(
+    {
+        term: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        documents: [documentEntrySchema],
+        df: {
+            type: Number,
+            default: 0,  // Document frequency (number of docs containing this term)
+        },
+    },
+    { timestamps: true }
+);
+
+const Index = mongoose.model('Index', indexSchema);
+
+module.exports = Index;
